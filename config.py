2956d79a8e92b235fecbc7c1257b890f11fc2459
danielbusbib <daniel.busbib@gmail.com>
2023-04-03 23:49:03 +0300

    config file - not done

---
+++ config.py
@@ -0,0 +1,58 @@
+TEAM_NAME = 'הפועל י-ם'
+HOME_STADIUM = 'ירושלים איצטדיון טדי'
+
+PLAYERS = {'אדבאיו אדליי': 'Adebayo ADELEYE',
+           'עומר אגבדיש': 'Omer AGVADISH',
+           'תומר אלטמן': 'Tomer ALTMAN',
+           'אווקה אשטה': 'Awaka ASHTA',
+           "אונדרז' באצ'ו": 'Ondrej BACO',
+           'גיא בדש': 'Guy BADASH',
+           'נואף בזיע': 'Nwaf BAZIAA',
+           'גולן בני': 'Golan Beni',
+           'אופק ביטון': 'Ofek BITON',
+           "ג'ורדן בוטאקה": 'Jordan BOTAKA',
+           'עמית גלזר': 'Amit GLAZER',
+           'נעם משה גרוס': 'Noam Moshe GROSS',
+           'פלג חמני': 'Peleg HAMANI',
+           "דור ג'אן": 'Dor JAN',
+           'גוני נאור': 'Goni NAOR',
+           'עומר קבילו': 'Omer KABILO',
+           'אלעד קפלן': 'Elad KAPLAN',
+           'יוראי מליח': 'Yorai MALIAC',
+           'נועם גיל מלמוד': 'Noam MELAMUD',
+           'נדב אביב נידם': 'Nadav NIDAM',
+           'איינאו פרדה': 'Aynao PRADA',
+           'רוי רביבו': 'Roy REVIVO',
+           'יהונתן שאבי': 'Yehonatan SHABI',
+           'הראל שלום': 'Harel SHALOM',
+           'ויליאם טוגי': 'William TOGUI',
+           'יואב תומר': 'Yoav Tomer',
+           "אילוז' יאו": 'Guy Eloge Koffi Yao',
+           'איתי זדה': 'Itay ZADA',
+           'אחמד איברהים': 'Ahmed Salman',
+           'דון סדריק': 'Cedric Franck Don',
+           'גיא חדידה': 'Guy Hadida',
+           "מאור ג'רסי": 'Maor GEASSI',
+           'קלטוס נומבייל': 'Cletus NOMBIL',
+           'וויליאם אגאדה': 'William AGADA',
+           'בן מיזן': 'Ben MIZAN',
+           'לירן אלמליח': 'Liran ELMALIAH',
+           'עידן שמש': 'Idan SHEMESH',
+           'רוסלן ברסקי': 'Roslan BARSKI',
+           'מידן יוסף כהן': 'Midan COHEN',
+           'גל מאיו': 'Gal MAYO',
+           "חאמד שאולאג'ה": 'Hamed SHOLAJA',
+           'איהאב שאמי': 'Ehab SHAMI',
+           'סולומון דניאל': 'Solomon DANIEL',
+           'אדוארדו גררו': 'Eduardo GUERREDO',
+           'ברייט אנובקרה': 'Bright ENOBAKHARE',
+           'אבירם זיאת': 'Aviram ZIAT'
+           }
+
+STADIUMS = {
+
+}
+
+TEAMS = {
+
+}
